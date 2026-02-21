328dab59cdaa28d5d7cdbd55dc35f0cf71ab8791
humoflife <markus.schweig@upbound.io>
2026-02-20 19:09:40 -0800

    replace types with model types
    Signed-off-by: humoflife <markus.schweig@upbound.io>

--- template/tests/e2etest-xstoragebucket-go/main.go
+++ template/tests/e2etest-xstoragebucket-go/main.go
@@ -2,14 +2,23 @@
 package main
 
 import (
-	metav1 "dev.upbound.io/models/io/k8s/meta/v1"
-	metav1alpha1 "dev.upbound.io/models/io/upbound/dev/meta/v1alpha1"
 	"encoding/json"
 	"fmt"
-	"k8s.io/utils/ptr"
 	"os"
+
+	"dev.upbound.io/models/com/example/platform/v1alpha1"
+	metacorev1 "dev.upbound.io/models/io/k8s/core/meta/v1"
+	corev1 "dev.upbound.io/models/io/k8s/core/v1"
+	metav1 "dev.upbound.io/models/io/k8s/meta/v1"
+	azv1beta1 "dev.upbound.io/models/io/upbound/azure/v1beta1"
+	metav1alpha1 "dev.upbound.io/models/io/upbound/dev/meta/v1alpha1"
+	"k8s.io/utils/ptr"
 	"sigs.k8s.io/yaml"
 )
+
+type e2eTestList struct {
+	Items []metav1alpha1.E2ETest `json:"items"`
+}
 
 func main() {
 	// Read Azure credentials from environment variable
@@ -24,17 +33,17 @@ func main() {
 	// Define the StorageBucket XR to deploy for E2E testing
 	// Note: XStorageBucket is cluster-scoped (no namespace)
 	manifests := resourcesToItems[metav1alpha1.E2ETestSpecManifestsItem](
-		map[string]interface{}{
-			"apiVersion": "platform.example.com/v1alpha1",
-			"kind":       "XStorageBucket",
-			"metadata": map[string]interface{}{
-				"name": "e2e-test-bucket",
+		&v1alpha1.XStorageBucket{
+			APIVersion: ptr.To(v1alpha1.XStorageBucketAPIVersionplatformExampleComV1Alpha1),
+			Kind:       ptr.To(v1alpha1.XStorageBucketKindXStorageBucket),
+			Metadata: &metav1.ObjectMeta{
+				Name: ptr.To("e2e-test-bucket"),
 			},
-			"spec": map[string]interface{}{
-				"parameters": map[string]interface{}{
-					"location":   "eastus",
-					"versioning": true,
-					"acl":        "private",
+			Spec: &v1alpha1.XStorageBucketSpec{
+				Parameters: &v1alpha1.XStorageBucketSpecParameters{
+					Location:   ptr.To("eastus"),
+					Versioning: ptr.To(true),
+					ACL:        ptr.To("private"),
 				},
 			},
 		},
@@ -43,31 +52,31 @@ func main() {
 	// Define extra resources: Secret and ProviderConfig
 	extraResources := resourcesToItems[metav1alpha1.E2ETestSpecExtraResourcesItem](
 		// Azure credentials Secret (created from environment variable)
-		map[string]interface{}{
-			"apiVersion": "v1",
-			"kind":       "Secret",
-			"metadata": map[string]interface{}{
-				"name":      "azure-creds",
-				"namespace": "upbound-system",
+		&corev1.Secret{
+			APIVersion: ptr.To(corev1.SecretAPIVersionV1),
+			Kind:       ptr.To(corev1.SecretKindSecret),
+			Metadata: &metacorev1.ObjectMeta{
+				Name:      ptr.To("azure-creds"),
+				Namespace: ptr.To("upbound-system"),
 			},
-			"stringData": map[string]interface{}{
+			StringData: &map[string]string{
 				"credentials": azureCreds,
 			},
 		},
 		// Azure ProviderConfig (references the Secret)
-		map[string]interface{}{
-			"apiVersion": "azure.upbound.io/v1beta1",
-			"kind":       "ProviderConfig",
-			"metadata": map[string]interface{}{
-				"name": "default",
+		&azv1beta1.ProviderConfig{
+			APIVersion: ptr.To(azv1beta1.ProviderConfigAPIVersionazureUpboundIoV1Beta1),
+			Kind:       ptr.To(azv1beta1.ProviderConfigKindProviderConfig),
+			Metadata: &metav1.ObjectMeta{
+				Name: ptr.To("default"),
 			},
-			"spec": map[string]interface{}{
-				"credentials": map[string]interface{}{
-					"source": "Secret",
-					"secretRef": map[string]interface{}{
-						"namespace": "upbound-system",
-						"name":      "azure-creds",
-						"key":       "credentials",
+			Spec: &azv1beta1.ProviderConfigSpec{
+				Credentials: &azv1beta1.ProviderConfigSpecCredentials{
+					Source: ptr.To(azv1beta1.ProviderConfigSpecCredentialsSourceSecret),
+					SecretRef: &azv1beta1.ProviderConfigSpecCredentialsSecretRef{
+						Namespace: ptr.To("upbound-system"),
+						Name:      ptr.To("azure-creds"),
+						Key:       ptr.To("credentials"),
 					},
 				},
 			},
@@ -87,9 +96,10 @@ func main() {
 			TimeoutSeconds:    ptr.To(600), // 10 minutes for real Azure resources
 		},
 	}
+
 	// Wrap in items array as expected by the test runner
-	output := map[string]interface{}{
-		"items": []interface{}{test},
+	output := e2eTestList{
+		Items: []metav1alpha1.E2ETest{test},
 	}
 	out, err := yaml.Marshal(output)
 	if err != nil {
@@ -98,20 +108,23 @@ func main() {
 	}
 	fmt.Print(string(out))
 }
-func toItem[T any](resource interface{}) T {
+
+func toItem[T any](resource any) T {
 	var item T
 	if err := convertViaJSON(&item, resource); err != nil {
 		panic(fmt.Sprintf("converting item: %v", err))
 	}
 	return item
 }
-func resourcesToItems[T any](resources ...interface{}) []T {
+
+func resourcesToItems[T any](resources ...any) []T {
 	items := make([]T, 0, len(resources))
 	for _, res := range resources {
 		items = append(items, toItem[T](res))
 	}
 	return items
 }
+
 func convertViaJSON(to, from any) error {
 	bs, err := json.Marshal(from)
 	if err != nil {
